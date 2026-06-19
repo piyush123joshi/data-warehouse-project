07c5ba5d75d26e706cacd3dbf672c40c953de451
piyush123joshi <89722494+piyush123joshi@users.noreply.github.com>
2026-06-19 18:18:10 +0530

    quality_silver.sql

--- tests/quality_silver.sql
+++ tests/quality_silver.sql
@@ -2,7 +2,8 @@
 ===============================================================================
 Quality Checks
 ===============================================================================
-Script Purpose:
+
+Purpose:
     This script performs various quality checks for data consistency, accuracy, 
     and standardization across the 'silver' layer. It includes checks for:
     - Null or duplicate primary keys.
@@ -10,18 +11,12 @@ Script Purpose:
     - Data standardization and consistency.
     - Invalid date ranges and orders.
     - Data consistency between related fields.
-
-Usage Notes:
-    - Run these checks after data loading Silver Layer.
-    - Investigate and resolve any discrepancies found during the checks.
-===============================================================================
 */
 
--- ====================================================================
 -- Checking 'silver.crm_cust_info'
 -- ====================================================================
+
 -- Check for NULLs or Duplicates in Primary Key
--- Expectation: No Results
 SELECT 
     cst_id,
     COUNT(*) 
@@ -30,7 +25,6 @@ GROUP BY cst_id
 HAVING COUNT(*) > 1 OR cst_id IS NULL;
 
 -- Check for Unwanted Spaces
--- Expectation: No Results
 SELECT 
     cst_key 
 FROM silver.crm_cust_info
@@ -41,11 +35,11 @@ SELECT DISTINCT
     cst_marital_status 
 FROM silver.crm_cust_info;
 
--- ====================================================================
+
 -- Checking 'silver.crm_prd_info'
 -- ====================================================================
 -- Check for NULLs or Duplicates in Primary Key
--- Expectation: No Results
+
 SELECT 
     prd_id,
     COUNT(*) 
@@ -53,37 +47,32 @@ FROM silver.crm_prd_info
 GROUP BY prd_id
 HAVING COUNT(*) > 1 OR prd_id IS NULL;
 
--- Check for Unwanted Spaces
--- Expectation: No Results
+    -- Check for Unwanted Spaces
 SELECT 
     prd_nm 
 FROM silver.crm_prd_info
 WHERE prd_nm != TRIM(prd_nm);
 
--- Check for NULLs or Negative Values in Cost
--- Expectation: No Results
+    -- Check for NULLs or Negative Values in Cost
 SELECT 
     prd_cost 
 FROM silver.crm_prd_info
 WHERE prd_cost < 0 OR prd_cost IS NULL;
 
--- Data Standardization & Consistency
+    -- Data Standardization & Consistency
 SELECT DISTINCT 
     prd_line 
 FROM silver.crm_prd_info;
 
--- Check for Invalid Date Orders (Start Date > End Date)
--- Expectation: No Results
+    -- Check for Invalid Date Orders (Start Date > End Date)
 SELECT 
     * 
 FROM silver.crm_prd_info
 WHERE prd_end_dt < prd_start_dt;
 
--- ====================================================================
 -- Checking 'silver.crm_sales_details'
 -- ====================================================================
--- Check for Invalid Dates
--- Expectation: No Invalid Dates
+    -- Check for Invalid Dates
 SELECT 
     NULLIF(sls_due_dt, 0) AS sls_due_dt 
 FROM bronze.crm_sales_details
@@ -92,16 +81,14 @@ WHERE sls_due_dt <= 0
     OR sls_due_dt > 20500101 
     OR sls_due_dt < 19000101;
 
--- Check for Invalid Date Orders (Order Date > Shipping/Due Dates)
--- Expectation: No Results
+    -- Check for Invalid Date Orders (Order Date > Shipping/Due Dates)
 SELECT 
     * 
 FROM silver.crm_sales_details
 WHERE sls_order_dt > sls_ship_dt 
    OR sls_order_dt > sls_due_dt;
 
--- Check Data Consistency: Sales = Quantity * Price
--- Expectation: No Results
+    -- Check Data Consistency: Sales = Quantity * Price
 SELECT DISTINCT 
     sls_sales,
     sls_quantity,
@@ -116,36 +103,34 @@ WHERE sls_sales != sls_quantity * sls_price
    OR sls_price <= 0
 ORDER BY sls_sales, sls_quantity, sls_price;
 
--- ====================================================================
+
 -- Checking 'silver.erp_cust_az12'
 -- ====================================================================
--- Identify Out-of-Range Dates
--- Expectation: Birthdates between 1924-01-01 and Today
+    -- Identify Out-of-Range Dates
 SELECT DISTINCT 
     bdate 
 FROM silver.erp_cust_az12
 WHERE bdate < '1924-01-01' 
    OR bdate > GETDATE();
 
--- Data Standardization & Consistency
+    -- Data Standardization & Consistency
 SELECT DISTINCT 
     gen 
 FROM silver.erp_cust_az12;
 
--- ====================================================================
+
 -- Checking 'silver.erp_loc_a101'
 -- ====================================================================
--- Data Standardization & Consistency
+    -- Data Standardization & Consistency
 SELECT DISTINCT 
     cntry 
 FROM silver.erp_loc_a101
 ORDER BY cntry;
 
--- ====================================================================
+
 -- Checking 'silver.erp_px_cat_g1v2'
 -- ====================================================================
--- Check for Unwanted Spaces
--- Expectation: No Results
+    -- Check for Unwanted Spaces
 SELECT 
     * 
 FROM silver.erp_px_cat_g1v2
